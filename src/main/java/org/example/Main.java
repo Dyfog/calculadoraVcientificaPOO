7c12820e6c4810f5a0b9c2edd2e3e5dbe99f80b0
Ztellar <b.salamanca01@ufromail.cl>
2023-04-13 14:03:49 -0400

    Actualización Métodos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -25,32 +25,43 @@ public class Main {
         arr3D[2] = ingresarSoloNumero();
         return arr3D;
     }
+    public static boolean comprobarCero(double[] lados) {
+        boolean comprobar = true;
+        for (int i = 0; i < lados.length; i++) {
+            if (lados[i] == 0) {
+                comprobar = false;
+                break;
+            }
+        } return comprobar;
+    }
     public static boolean comprobarTriangulo(double[] lados) {
-        if (lados[0] == 0 || lados[1] == 0 || lados[2] == 0) {
+        double hipotenusa = Math.sqrt(Math.pow(lados[0], 2) + Math.pow(lados[1], 2));
+        if (hipotenusa == 0) {
             return false;
         }
         return true;
     }
-    public static double perimetroCuadrado(double[] lado){
-        return lado[1]*4;
+    public static double perimetroCuadrado(double[] lados){
+        return lados[1]*4;
     }
     public static double perimetroRectangulo(double[] lados){
         return lados[0]*2 + lados[1]*2;
     }
-    public static double perimetroCirculo(double[] lado){
-        return Math.PI*lado[0];
+    public static double perimetroCirculo(double[] lados){
+        return Math.PI*lados[0];
     }
     public static double perimetroEsfera(double[] lados){
         return 2*Math.PI*lados[1];
     }
-    public static double perimetroCubo(double[] lado){
-        return 12*lado[0];
+    public static double perimetroCubo(double[] lados){
+        return 12*lados[0];
     }
     public static double perimetroCono(double[]lados){
-        return ;
+        double hipotenusa = Math.sqrt(Math.pow(lados[0], 2) + Math.pow(lados[1], 2));
+        return 2*Math.PI*lados[0] + hipotenusa;
     }
-    public static double areaCuadrado(double lado1){
-        return Math.pow(lado1, 2);
+    public static double areaCuadrado(double lados){
+        return Math.pow(lados, 2);
     }
     public static double areaRectangulo(double[] lados){
         return lados[0]*lados[1];
@@ -65,16 +76,17 @@ public class Main {
         return 6*(Math.pow(lados[0], 2));
     }
     public static double areaCono(double[] lados){
-
+        double hipotenusa = Math.sqrt(Math.pow(lados[0], 2) + Math.pow(lados[1], 2));
+        return Math.PI*lados[0]*(lados[0]+hipotenusa);
     }
     public static double volumenEsfera(double[] lados){
-
+        return (4/3)*Math.PI*Math.pow(lados[0], 3);
     }
     public static double volumenCubo(double[] lados){
         return Math.pow(lados[0], 3);
     }
     public static double volumenCono(double[] lados){
-
+        return (1/3)*lados[1]*Math.PI*Math.pow(lados[0], 3);
     }
     public static double ingresarSoloNumero() {
         Scanner in = new Scanner(System.in);
